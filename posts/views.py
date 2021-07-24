d11076fdc6474d0c761fad033d08f68f94f1ff0d
Kairat <toroevkajrat@gmail.com>
2021-07-24 20:57:50 +0600

    Refactor view classes

--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from posts.models import Post
 from posts.forms import PostForm, SearchForm
 from comments.forms import CommentForm
+from .utils import ObjectCreateMixin, ObjectDeleteMixin, ObjectUpdateMixin
 
-def post_detail_view(request,id):
+
+def post_detail_view(request, id):
     post = get_object_or_404(Post, id=id)
     comments = post.comments.filter(active=True)
     if request.method == 'POST':
@@ -14,12 +16,12 @@ def post_detail_view(request,id):
             new_comment.post = post
             new_comment.save()
             return render(request, 'posts/post_detail.html', {'post': post, 'comments': comments
-            , 'comment_form': comment_form, 'new_comment': new_comment})
+                , 'comment_form': comment_form, 'new_comment': new_comment})
 
     else:
         comment_form = CommentForm()
-        return render(request,'posts/post_detail.html', {'post': post,'comments': comments
-                                                     , 'comment_form': comment_form})
+        return render(request, 'posts/post_detail.html', {'post': post, 'comments': comments
+            , 'comment_form': comment_form})
 
 
 def posts_list_view(request):
@@ -31,49 +33,21 @@ def posts_list_view(request):
         if search_form.is_valid():
             search_param = search_form.cleaned_data.get('search_param')
             filtered_posts = Post.objects.filter(title__icontains=search_param)
-            return render(request,'posts/index.html',context={'posts': filtered_posts})
+            return render(request, 'posts/index.html', context={'posts': filtered_posts})
 
 
+class PostCreateView(View, ObjectCreateMixin):
+    form = PostForm
+    template = 'posts/post_create.html'
 
 
-class PostCreateView(View):
-
-    def get(self, request):
-        form = PostForm()
-        return render(request, 'posts/post_create.html', context={'form': form})
-
-    def post(self, request):
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save()
-            return redirect(post)
-        return render(request, 'posts/post_create.html', context={'form': form})
+class PostUpdateView(View, ObjectUpdateMixin):
+    obj_class = Post
+    template = 'posts/post_update.html'
+    bound_form = PostForm
 
 
-class PostUpdateView(View):
-
-    def get(self, request, id):
-        post = get_object_or_404(Post, id=id)
-        bound_form = PostForm(instance=post)
-        return render(request, 'posts/post_update.html', context={'form': bound_form, 
-                                                                  'post': post})
-
-    def post(self, request, id):
-        post = get_object_or_404(Post, id=id)
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save()
-            return redirect(post)
-        return render(request, 'posts/post_update.html', context={'form': form, 'post': post})
-
-
-class PostDeleteView(View):
-
-    def get(self, request, id):
-        post = get_object_or_404(Post, id=id)
-        return render(request, 'posts/post_delete.html', context={'post': post})
-
-    def post(self, request, id):
-        post = get_object_or_404(Post, id=id)
-        post.delete()
-        return redirect(reverse('posts_list_url'))
+class PostDeleteView(View, ObjectDeleteMixin):
+    template = 'posts/post_delete.html'
+    obj_class = Post
+    redirect_template = 'posts_list_url'
--- tags/views.py
+++ tags/views.py
@@ -4,7 +4,7 @@ from django.views import View
 
 from tags.models import Tag
 from tags.forms import TagForm
-
+from posts.utils import ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 
 def tags_list_view(request):
     tags = Tag.objects.all()
@@ -16,44 +16,16 @@ def tag_detail_view(request, id):
     return render(request, 'tags/tag_detail.html', context={'tag': tag})
 
 
-class TagCreateView(View):
+class TagCreateView(View, ObjectCreateMixin):
+    form = TagForm
+    template ='tags/tag_create.html'
 
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'tags/tag_create.html', context={'form': form})
+class TagUpdateView(View,ObjectUpdateMixin):
+    obj_class = Tag
+    template = 'tags/tag_update.html'
+    form = TagForm
+class TagDeleteView(View, ObjectDeleteMixin):
+    template = 'tags/tag_delete.html'
+    obj_class = Tag
+    redirect_template = 'tags_list_url'
 
-    def post(self, request):
-        form = TagForm(request.POST)
-        if form.is_valid():
-            tag = form.save()
-            return redirect(tag)
-        return render(request, 'tags/tag_create.html', context={'form': form})
-
-
-class TagUpdateView(View):
-
-    def get(self, request, id):
-        post = get_object_or_404(Tag, id=id)
-        bound_form = TagForm(instance=post)
-        return render(request, 'tags/tag_update.html', context={'form': bound_form,
-                                                                  'post': post})
-
-    def post(self, request, id):
-        post = get_object_or_404(Tag, id=id)
-        form = TagForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save()
-            return redirect(post)
-        return render(request, 'tags/tag_update.html', context={'form': form, 'post': post})
-
-
-class TagDeleteView(View):
-
-    def get(self, request, id):
-        post = get_object_or_404(Tag, id=id)
-        return render(request, 'tags/tag_delete.html', context={'post': post})
-
-    def post(self, request, id):
-        post = get_object_or_404(Tag, id=id)
-        post.delete()
-        return redirect(reverse('tags_list_url'))
